084ac60c0d4bbee731788ffee5875318db45cba1
Sachin Mahesh <sachin.mahesh@esb.ie>
2023-12-22 14:27:21 +0000

    find largest element completed

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,12 @@
 */
 
 function findLargestElement(numbers) {
-    
+  let max = numbers[0];
+  numbers.slice(1).map((ele) => {
+    ele > max ? (max = ele) : null;
+  });
+  return max;
 }
 
+findLargestElement([3, 7, 2, 9, 1]);
 module.exports = findLargestElement;
