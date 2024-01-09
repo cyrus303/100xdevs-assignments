19bca79b1d6d572fb1173282f228d7a9fe5fbe68
Sachin Mahesh <sachin.mahesh@esb.ie>
2024-01-09 17:04:56 +0000

    working on user authentication

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose
+  .connect(
+    'mongodb+srv://Sachin:oiIYTc6Xva29zWqT@mongodb.7d1cjqv.mongodb.net/'
+  )
+  .then(console.log('connected to DB'));
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
+  username: String,
+  password: String,
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+  // Schema definition here
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
@@ -21,7 +27,7 @@ const User = mongoose.model('User', UserSchema);
 const Course = mongoose.model('Course', CourseSchema);
 
 module.exports = {
-    Admin,
-    User,
-    Course
-}
+  Admin,
+  User,
+  Course,
+};
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,29 @@
-const { Router } = require("express");
-const adminMiddleware = require("../middleware/admin");
+const {Router} = require('express');
+const adminMiddleware = require('../middleware/admin');
+const {Admin} = require('../db');
 const router = Router();
 
 // Admin Routes
 router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+  // Implement admin signup logic
+  const {username, password} = req.body;
+
+  Admin.create({
+    username: username,
+    password: password,
+  });
+
+  res.json({
+    message: 'Admin created successfully',
+  });
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+  // Implement course creation logic
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+  // Implement fetching all courses logic
 });
 
 module.exports = router;
